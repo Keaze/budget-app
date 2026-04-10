926c08c1c9ab41e3429166908bdbb3fbf68d6310
Daniel <danielwiese@sent.com>
2026-04-11 00:27:30 +0200

    feat: add i18next and translation files (en + de)

---
+++ frontend/src/i18n.js
@@ -0,0 +1,16 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import en from './locales/en.json'
+import de from './locales/de.json'
+
+i18n.use(initReactI18next).init({
+  lng: localStorage.getItem('budget_language') ?? 'en',
+  fallbackLng: 'en',
+  resources: {
+    en: { translation: en },
+    de: { translation: de },
+  },
+  interpolation: { escapeValue: false },
+})
+
+export default i18n
